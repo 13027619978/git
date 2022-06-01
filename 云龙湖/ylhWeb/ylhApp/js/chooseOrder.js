703e38f8ee77a7e2b3de40426447227166b329f7
13027619978 <13027619978@163.com>
2022-06-01 08:39:46 +0800

    Revert "云龙湖上传"
    This reverts commit e87594b407b3ed4e2e42024f02840a2d6369ac9a.

--- 云龙湖/ylhWeb/ylhApp/js/chooseOrder.js
+++
@@ -1,9 +0,0 @@
-function orderClick(deviceType){
-	if(deviceType == '1'){
-		window.location.href = 'order.html?deviceType=1';
-	}else if(deviceType == '2'){
-		window.location.href = 'carTicketsOrder.html';
-	}else{
-		window.location.href = 'boatTicketsOrder.html';
-	}
-};
--- 云龙湖/ylhWeb/ylhApp/js/js/chooseOrder.js
+++
@@ -1,7 +0,0 @@
-function orderClick(deviceType){
-	if(deviceType == '1' || deviceType == '2'){
-		window.location.href = 'order.html?deviceType=' + deviceType;
-	}else{
-		window.location.href = 'boatTicketsOrder.html';
-	}
-};
